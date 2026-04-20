a8bb98f1276f949ddfb72a36c5476ce3829c3c25
EatBreadMan <103365994+EatBreadMan@users.noreply.github.com>
2026-04-20 18:03:58 +0300

    retry

--- src/main/java/com/p2ps/repository/StoreInventoryMapRepository.java
+++ src/main/java/com/p2ps/repository/StoreInventoryMapRepository.java
@@ -1,13 +1,12 @@
 package com.p2ps.repository;
 
-import com.p2ps.entity.StoreInventoryMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
+import com.p2ps.model.StoreInventoryMap;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
