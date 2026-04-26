8ec68c4655edb6c1a7faffa60f0877ba2ef9bf20
oliviaa28 <156539318+oliviaa28@users.noreply.github.com>
2026-04-26 21:34:02 +0300

    fix(telemetry): remove CompoundIndexes wrapper, use repeatable annotations

--- src/main/java/com/p2ps/telemetry/model/TelemetryRecord.java
+++ src/main/java/com/p2ps/telemetry/model/TelemetryRecord.java
@@ -6,16 +6,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
-import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "telemetry_records")
-@CompoundIndexes({
-        @CompoundIndex(def = "{'storeId': 1, 'itemId': 1}"),
-        @CompoundIndex(name = "unique_device_timestamp", def = "{'deviceId': 1, 'timestamp': 1}", unique = true)
-})
+@CompoundIndex(def = "{'storeId': 1, 'itemId': 1}")
+@CompoundIndex(name = "unique_device_timestamp", def = "{'deviceId': 1, 'timestamp': 1}", unique = true)
+
 public class TelemetryRecord {
 
     @Id
