4a020ce45753aa8651a16b654db2e6b0359a8407
supernova <matei.gurzu@gmail.com>
2026-04-28 14:15:26 +0300

    Remove unused import in PresenceController
    Removed unused import statement for SendTo annotation.

--- src/main/java/com/p2ps/controller/PresenceController.java
+++ src/main/java/com/p2ps/controller/PresenceController.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
