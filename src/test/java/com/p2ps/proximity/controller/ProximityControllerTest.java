2115353b4b605b2ad82670c1c4882cc9721af709
oliviaa28 <156539318+oliviaa28@users.noreply.github.com>
2026-05-08 21:11:50 +0300

    fix(test): add @ActiveProfiles(test) and disable scheduling in test profile

--- src/test/java/com/p2ps/proximity/controller/ProximityControllerTest.java
+++ src/test/java/com/p2ps/proximity/controller/ProximityControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProximityControllerTest {
 
     @Autowired
